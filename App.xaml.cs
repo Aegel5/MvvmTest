c9672659849f752232ecc55af3e29877a7125a43
alex <alex@alex.ru>
2019-04-20 20:51:48 +0300

    first version

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvvmTest.VM;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,15 @@ namespace MvvmTest
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+            var viewModel = new MainVM();
+            window.DataContext = viewModel;
+
+            window.Show();
+        }
     }
 }
---
+++ VM/MainVM.cs
@@ -0,0 +1,23 @@
+﻿using MvvmTest.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MvvmTest.VM
+{
+    class MainVM : ViewModelBase
+    {
+        RelayCommand _NewUser;
+        public MainVM()
+        {
+            _NewUser = new RelayCommand(param => MessageBox.Show("Create new user"));
+            NewUser = _NewUser;
+        }
+        public ICommand NewUser { get; private set; }
+    }
+}
